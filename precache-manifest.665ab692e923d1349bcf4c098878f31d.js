f12846c87f144dacab7883e5872fa4993cce854b
github_colsen12 <colsen788@gmail.com>
2020-07-05 19:19:57 -0400

    Updates

---
+++ precache-manifest.665ab692e923d1349bcf4c098878f31d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6680f4a74318b4065445b5c664e02d61",
+    "url": "/React-Recipe/index.html"
+  },
+  {
+    "revision": "02611b624a2d921fc89c",
+    "url": "/React-Recipe/static/css/main.2da6cbb1.chunk.css"
+  },
+  {
+    "revision": "ee6dd24720cf5df03272",
+    "url": "/React-Recipe/static/js/2.9e0009c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Recipe/static/js/2.9e0009c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02611b624a2d921fc89c",
+    "url": "/React-Recipe/static/js/main.acdd671a.chunk.js"
+  },
+  {
+    "revision": "e4b798ee4c92e64d7344",
+    "url": "/React-Recipe/static/js/runtime-main.bb5353f1.js"
+  },
+  {
+    "revision": "1f98a097816a0ae2c9aab0a30fb25339",
+    "url": "/React-Recipe/static/media/background.1f98a097.jpg"
+  }
+]);
